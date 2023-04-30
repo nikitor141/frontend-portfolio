acde007a1f298805508db8e973ac7986f1e9f236
nikitor141 <nikitor141@gmail.com>
2023-04-30 19:28:17 +0300

    desktop

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,5 @@
 import * as webpTest from './modules/iswebp.js';
+import IMask from 'imask';
 webpTest.isWebp()
 
 //scrollMargin
@@ -32,3 +33,49 @@ const setThemeCallback = (mq) => {
 }
 darkThemeMq.addEventListener('change', setThemeCallback);
 setThemeCallback(darkThemeMq);
+
+//phone mask
+
+let dynamicMask = IMask(
+   contact,
+   {
+      mask: [
+         {
+            mask: '+7 000 000 00 00'
+         },
+         {
+            mask: /^\S*@?\S*$/
+         }
+      ]
+   });
+
+//form dropdown
+
+document.querySelectorAll('.form__dropdown').forEach(dropdown => {
+   let dropdownOptions = dropdown.querySelector('.form__dropdown-options');
+   let optionsHeight = 0;
+   let dropdownHeightFunc = () => {
+      if (dropdown.classList.contains('active')) {
+         dropdownOptions.style.height = optionsHeight + 'px';
+      } else {
+         dropdownOptions.removeAttribute('style');
+      }
+   }
+   dropdown.querySelectorAll('.form__dropdown-option').forEach(option => {
+      optionsHeight += option.offsetHeight;
+      option.addEventListener('click', () => {
+         connect.value = option.innerHTML;
+      });
+   });
+   dropdown.addEventListener('click', () => {
+      dropdown.classList.toggle('active');
+      dropdownHeightFunc();
+   });
+   window.addEventListener('resize', () => {
+      optionsHeight = 0;
+      dropdown.querySelectorAll('.form__dropdown-option').forEach(option => {
+         optionsHeight += option.offsetHeight;
+      });
+      dropdownHeightFunc()
+   })
+});
